1eb02a96a6c362d9a17307fef8616839ecd57ca2
Tyler Davis <datadazer@gmail.com>
2022-04-09 10:11:14 -0700

    refactor to generate square objects

--- main.js
+++ main.js
@@ -21,7 +21,8 @@ class Board {
     // this.fen = "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"
     // this.fen = "3k4/5ppp/2q5/3p2r1/8/1Q3P2/P4P1P/3R3K w - - 0 1"
     this.board = [];
-    this.squareWidth = 70;
+    this.squareSize = 70;
+    this.colors = {white: '#ddbc93', black: '#955431'}
   }
 
   initialize() {
@@ -33,35 +34,47 @@ class Board {
       rank.split('').map( square => ( !isNaN(square) ? '' : square ))
     ))
 
-    this.generateBoardDisplay()
+    this.generateSquares();
     this.generatePieces();
   }
 
-  generateBoardDisplay() {
+  generateSquares() {
     ctx.clearRect(0, 0, 560, 560);
-    for (let i = 0; i < 8; i++) {
-      for (let j = 0; j < 8; j++) {
-        let squareColor = (i+j) % 2 === 0 ? '#ddbc93' : '#955431';
-        ctx.fillStyle = squareColor;
-        ctx.fillRect(i*this.squareWidth, j*this.squareWidth, this.squareWidth, this.squareWidth);
+    for (let x = 0; x < 8; x++) {
+      for (let y = 0; y < 8; y++) {
+        let color = (x + y) % 2 === 0 ? this.colors.white : this.colors.black;
+        const square = new Square({x, y, size: this.squareSize, color});
+        square.draw();
       }
     }
   }
 
   generatePieces() {
     ctx.font = '48px serif';
-    this.board.forEach( (rank, i) => {
-      rank.forEach( (square, j) => {
-        if (pieceTable.hasOwnProperty(square.toLowerCase())) {
-          ctx.fillStyle = square === square.toLowerCase() ? '#000' : '#fff';
-          ctx.fillText(pieceTable[square.toLowerCase()], j * this.squareWidth + 10, i * this.squareWidth + 50);
+    this.board.forEach( (rank, x) => {
+      rank.forEach( (piece, y) => {
+        if (pieceTable.hasOwnProperty(piece.toLowerCase())) {
+          ctx.fillStyle = piece === piece.toLowerCase() ? '#000' : '#fff';
+          ctx.fillText(pieceTable[piece.toLowerCase()], y * this.squareSize + 10, x * this.squareSize + 50);
         }
       })
     })
   }
 }
 
+class Square {
+  constructor({x, y, size, color}) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.color = color
+  }
+
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x * this.size, this.y * this.size, this.size, this.size);
+  }
+}
+
 let board = new Board;
 board.initialize();
-// board.fen = "3k4/5ppp/2q5/3p2r1/8/1Q3P2/P4P1P/3R3K w - - 0 1";
-// board.initialize();
